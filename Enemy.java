4d206346dfabde17e17f8cfa6c5b8f5bb033de47
mandaapriliani <mandaapriliani@student.ub.ac.id>
2023-05-10 21:34:27 +0700

    manda

---
+++ Enemy.java
@@ -0,0 +1,8 @@
+public class Enemy extends Character {
+    public void move(){
+        System.out.println("Apriliani");
+    }
+    public void move(int step){
+        System.out.println("Aurel " + step);
+    }
+}
---
+++ Hero.java
@@ -0,0 +1,8 @@
+public class Hero extends Character{
+    public void move(){
+        System.out.println("Nurul");
+    }
+    public void move(String direction){
+        System.out.println("Lidya " + direction);
+    }
+}
